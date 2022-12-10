5da19dda1b00855667a564e181694c6e5e842b7b
TemateuRoslyn <TemateuRoslyn@users.noreply.github.com>
2022-12-10 12:22:26 +0000

    deploy: dacf56432416f4048a28c874b43be7333fac8c74

--- src/app/containers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './default-layout/default-layout.component';
-export * from './default-layout/footer/footer.component';
-export * from './default-layout/navbar/navbar.component';
-export * from './default-layout/scrool/scrool.component';
-export * from './default-layout/tabs';
--- src/app/pages/home/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './home.module';
-export * from './home-routing.module';
-export * from './components/index/index.component';
-export * from './components/index/index.component';
